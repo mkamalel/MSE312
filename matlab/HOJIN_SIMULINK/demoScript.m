ded004a33dd6787d8f63b343cfa84957672297ef
Mahmoud Kamaleldin <amahmoud98@gmail.com>
2021-08-04 21:39:23 -0700

    Stop simulation when y=0 and create demo script

--- matlab/HOJIN_SIMULINK/demoScript.m
+++ matlab/HOJIN_SIMULINK/demoScript.m
@@ -3,12 +3,12 @@ open_system('Quintic_Controller')
 Simulation_Time = 0.7;
 
 range_results = [];
-for x_desired = 1.2:0.01:1.5
+for x_desired = [0.24, 0.67, 1.44]
     set_param('Quintic_Controller/x_desired','Value', num2str(x_desired));
     output = sim('Quintic_Controller', Simulation_Time);
     x_landing = getBallPos(output.ball_y, output.ball_x);
     
     range_results = [range_results; [x_desired, x_landing, x_landing-x_desired]]; 
-    disp(x_landing);
+    fprintf("X Desired: %f, X Landing = %f, Error: %f\n", x_desired, x_landing, x_landing-x_desired);
 end
     
