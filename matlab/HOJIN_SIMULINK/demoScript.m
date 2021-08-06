fe380d3deca9516293bf7eed83405df76f9e1205
Ho Jin Chang <hojinc@sfu.ca>
2021-08-05 17:43:01 -0700

    Updated demoscript.m

--- matlab/HOJIN_SIMULINK/demoScript.m
+++ matlab/HOJIN_SIMULINK/demoScript.m
@@ -1,14 +1,38 @@
 open_system('Quintic_Controller')
+run('Parameters.m')
 
-Simulation_Time = 0.7;
+Simulation_Time = 0.65;
 
+i = 1;
 range_results = [];
-for x_desired = [0.24, 0.67, 1.44]
+
+for x_desired = [0.47, 0.66, 1.29]
+    
     set_param('Quintic_Controller/x_desired','Value', num2str(x_desired));
     output = sim('Quintic_Controller', Simulation_Time);
+    
     x_landing = getBallPos(output.ball_y, output.ball_x);
+    y_max = max(output.ball_y.data());
     
-    range_results = [range_results; [x_desired, x_landing, x_landing-x_desired]]; 
-    fprintf("X Desired: %f, X Landing = %f, Error: %f\n", x_desired, x_landing, x_landing-x_desired);
+    total_power = output.total_power.data(find(output.total_power.data(), 1, 'last'));
+    return_time = output.return_time.data(find(output.return_time.data(), 1, 'last'));
+    
+    range_results = [range_results; [x_desired, x_landing, x_landing-x_desired, y_max,total_power, return_time]]; 
+
+    fprintf("\nTest " + i + '\n');
+    fprintf(('x_desired = %.2f m\n'), x_desired);
+    fprintf(('x_landing = %.2f m\n'), x_landing);
+    fprintf(('y_max = %.2f m\n'), y_max);
+    fprintf(('total_power = %.2f W\n'), total_power);
+    fprintf(('return_time = %.2f s\n'), return_time);
+    
+    
+    i = i+1;
+
 end
-    
+
+
+   
+
+% Turns off a warning message
+% warning('off','sm:sli:setup:compile:LocalSolverNotSupported')
