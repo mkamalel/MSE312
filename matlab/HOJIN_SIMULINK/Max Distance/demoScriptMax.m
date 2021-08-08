4ac8e23198ca3df10ade689447baa563a09162b0
Ho Jin Chang <hojinc@sfu.ca>
2021-08-08 12:00:55 -0700

    Latest Files

--- matlab/HOJIN_SIMULINK/Max Distance/demoScriptMax.m
+++ matlab/HOJIN_SIMULINK/Max Distance/demoScriptMax.m
@@ -4,7 +4,7 @@ warning('off','sm:sli:setup:compile:LocalSolverNotSupported')
 open_system('Quintic_Controller_max_distance')
 run('Parameters.m')
 
-Simulation_Time = [0.95, 1.5];
+Simulation_Time = 5;
 
 i = 1;
 range_results = [];
@@ -15,15 +15,16 @@ for W_cruise = [40, 40]
     
     set_param('Quintic_Controller_max_distance/test_num','Value', num2str(i));
     set_param('Quintic_Controller_max_distance/W_cruise','Value', num2str(W_cruise));
-    output = sim('Quintic_Controller_max_distance', Simulation_Time(i));
+    output = sim('Quintic_Controller_max_distance', Simulation_Time());
     
     x_landing = getBallPos(output.ball_y, output.ball_x);
     y_max = max(output.ball_y.data()) - 0.02848783783;
     
     total_power = output.total_power.data(find(output.total_power.data(), 1, 'last'));
-    return_time = output.return_time.data(find(output.return_time.data(), 1, 'last'));
+    return_time_1 = output.return_time.data(find(output.return_time.data(), 1, 'last'));
+    return_time_2 = output.return_time2.data(find(output.return_time2.data(), 1, 'last'));
     
-    range_results = [range_results; [W_cruise, x_landing, y_max, total_power, return_time]]; 
+    range_results = [range_results; [x_landing, y_max, total_power, return_time_1, return_time_2]]; 
 
     if i == 1
         fprintf('\nTest Max Distance\n');
@@ -31,11 +32,51 @@ for W_cruise = [40, 40]
         fprintf('\nTest Max Height\n');
     end
     
-    fprintf(('w_cruise = %.2f rad/s\n'), W_cruise);
-    fprintf(('x_landing = %.2f m\n'), x_landing);
-    fprintf(('y_max = %.2f m\n'), y_max);
-    fprintf(('total_power = %.2f W\n'), total_power);
-    fprintf(('return_time = %.2f s\n'), return_time);
+    fprintf("\nTest " + num2str(i) + '\n');
+    fprintf(('x_max = %.4f m\n'), x_landing);
+    fprintf(('y_max = %.4f m\n'), y_max);
+    fprintf(('total_power = %.4f W\n'), total_power);
+    fprintf(('return_time_1 = %.4f s\n'), return_time_1);
+    fprintf(('return_time_2 = %.4f s\n'), return_time_2);
+    
+    
+    figure();
+    plot_title = sprintf("Test %d | x max = %.2f | y max = %.2f | total power = %.2f | return time 1 = %.2f, | return time 2 = %.2f",...
+                        i, x_landing, y_max, total_power, return_time_1, return_time_2);
+
+    plot(output.ball_x.Data+x0, output.ball_y.Data+y0);
+    title(plot_title);
+    ylabel('Y position (m)');
+    xlabel('X position (m)');
+    
+    figure();
+    plot_title = sprintf("Test %d", i);
+    
+    subplot(2,2,1)
+    plot(output.arm_pos);
+    title("Arm Position");
+    ylabel('Angular Position (rad)');
+    xlabel('Time (s)');    
+
+    subplot(2,2,2)
+    plot(output.arm_vel);
+    title("Arm Velocity");
+    ylabel('Angular Speed (rad/s)');
+    xlabel('Time (s)');    
+
+    subplot(2,2,3)
+    plot(output.arm_accel);
+    title("Arm Acceleration");
+    ylabel('Angular Acceleration (rad/s2)');
+    xlabel('Time (s)'); 
+    
+    subplot(2,2,4)
+    plot(output.total_power);
+    title('Total Power');
+    ylabel('Power (W)');
+    xlabel('Time (s)');
+    
+    sgtitle(plot_title) 
     
     i = i+1;
 end
--- matlab/HOJIN_SIMULINK/demoScript.m
+++ matlab/HOJIN_SIMULINK/demoScript.m
@@ -8,9 +8,13 @@ run('Parameters.m')
 Simulation_Time = 1;    % Simulation times for 0.5, 1.2, 1.5 m
 
 i = 1;
-range_results_sample = [];
-% [0.30, 1.01, 1.35]
-for x_desired = 0.2:0.01:1.5
+range_results = [];
+
+fprintf('Angle Noise = %f hz\n', Angle_noise);
+fprintf('Velocity Noise = %f hz\n', Velocity_noise);
+fprintf('Current Noise = %f hz\n', Current_noise);
+
+for x_desired = [0.30, 1.01, 1.35]
     
     set_param('Quintic_Controller/x_desired','Value', num2str(x_desired));
     output = sim('Quintic_Controller', Simulation_Time);
@@ -19,48 +23,58 @@ for x_desired = 0.2:0.01:1.5
     y_max = max(output.ball_y.data()) - 0.02848783783;
     
     total_power = output.total_power.data(find(output.total_power.data(), 1, 'last'));
-    return_time = output.return_time.data(find(output.return_time.data(), 1, 'last'));
+    return_time_1 = output.return_time.data(find(output.return_time.data(), 1, 'last'));
+    return_time_2 = output.return_time2.data(find(output.return_time2.data(), 1, 'last'));
     
-    range_results_sample = [range_results_sample; [x_desired, x_landing, x_landing-x_desired, y_max, total_power, return_time]]; 
+    range_results = [range_results; [x_desired, x_landing, x_landing-x_desired, y_max, total_power, return_time_1, return_time_2]]; 
 
-    fprintf("\nTest " + i + '\n');
-    fprintf(('x_desired = %.2f m\n'), x_desired);
-    fprintf(('x_landing = %.2f m\n'), x_landing);
-    fprintf(('y_max = %.2f m\n'), y_max);
-    fprintf(('total_power = %.2f W\n'), total_power);
-    fprintf(('return_time = %.2f s\n'), return_time);
-%     
-%     figure();
-%     plot_title = sprintf("Test %d | x desired: %.2f | x landing = %.2f | y max = %.2f | total power = %.2f | return time = %.2f",...
-%                         i, x_desired, x_landing, y_max, total_power, return_time);
-% 
-%     plot(output.ball_x.Data+x0, output.ball_y.Data+y0);
-%     title(plot_title);
-%     ylabel('Y position (m)');
-%     xlabel('X position (m)');
-%     
-%     figure();
-%     plot_title = sprintf("Test %d", i);
-%     
-%     subplot(3,1,1)
-%     plot(output.arm_pos);
-%     title("Arm Position");
-%     ylabel('Angular Position (rad)');
-%     xlabel('Time (s)');    
-% 
-%     subplot(3,1,2)
-%     plot(output.arm_vel);
-%     title("Arm Velocity");
-%     ylabel('Angular Speed (rad/s)');
-%     xlabel('Time (s)');    
-% 
-%     subplot(3,1,3)
-%     plot(output.arm_accel);
-%     title("Arm Acceleration");
-%     ylabel('Angular Acceleration (rad/s2)');
-%     xlabel('Time (s)'); 
-%     
-%     sgtitle(plot_title) 
+    fprintf("\nTest " + num2str(i) + '\n');
+    fprintf(('x_desired = %.4f m\n'), x_desired);
+    fprintf(('x_landing = %.4f m\n'), x_landing);
+    fprintf(('x_Error = %.4f m\n'), x_landing-x_desired);
+    fprintf(('y_max = %.4f m\n'), y_max);
+    fprintf(('total_power = %.4f W\n'), total_power);
+    fprintf(('return_time_1 = %.4f s\n'), return_time_1);
+    fprintf(('return_time_2 = %.4f s\n'), return_time_2);
+    
+    figure();
+    plot_title = sprintf("Test %d | x desired: %.2f | x landing = %.2f | y max = %.2f | total power = %.2f | return time 1 = %.2f, | return time 2 = %.2f",...
+                        i, x_desired, x_landing, y_max, total_power, return_time_1, return_time_2);
+
+    plot(output.ball_x.Data+x0, output.ball_y.Data+y0);
+    title(plot_title);
+    ylabel('Y position (m)');
+    xlabel('X position (m)');
+    
+    figure();
+    plot_title = sprintf("Test %d", i);
+    
+    subplot(2,2,1)
+    plot(output.arm_pos);
+    title("Arm Position");
+    ylabel('Angular Position (rad)');
+    xlabel('Time (s)');    
+
+    subplot(2,2,2)
+    plot(output.arm_vel);
+    title("Arm Velocity");
+    ylabel('Angular Speed (rad/s)');
+    xlabel('Time (s)');    
+
+    subplot(2,2,3)
+    plot(output.arm_accel);
+    title("Arm Acceleration");
+    ylabel('Angular Acceleration (rad/s2)');
+    xlabel('Time (s)'); 
+    
+    subplot(2,2,4)
+    plot(output.total_power);
+    title('Total Power');
+    ylabel('Power (W)');
+    xlabel('Time (s)');
+    
+    sgtitle(plot_title) 
+    
     
     i = i+1;
 
